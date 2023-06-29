b09856c1b0f25ee915955e363e027f15356dbcbf
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-06-29 22:17:21 +0300

    inherit for car

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,23 @@
+import Car from './10-car.js';
+
+class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = Car[Symbol.species];
+    return new Species(this.brand, this.motor, this.color);
+  }
+}
+
+export default EVCar;
