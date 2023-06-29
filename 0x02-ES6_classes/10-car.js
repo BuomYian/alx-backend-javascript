a41fecb5974821a25e96c7a2bcb9ee33c11a0312
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-06-29 22:11:12 +0300

    Species

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,30 +1,45 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-class Car {
+export default class Car {
+  // Initializing the constructor
   constructor(brand, motor, color) {
-    this[_brand] = brand;
-    this[_motor] = motor;
-    this[_color] = color;
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
+  //   Setter and getter for brand
   get brand() {
-    return this[_brand];
+    return this._brand;
   }
 
+  set brand(value) {
+    this._brand = value;
+  }
+
+  //   Setter and getter for mottor
   get motor() {
-    return this[_motor];
+    return this._motor;
   }
 
+  set motor(value) {
+    this._motor = value;
+  }
+
+  //   Setter and getter for color
   get color() {
-    return this[_color];
+    return this._color;
   }
 
+  set color(value) {
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  //   Defining a method
   cloneCar() {
-    const { brand, motor, color } = this;
-    return new Car(brand, motor, color);
+    const Species = this.constructor[Symbol.species];
+    return new Species();
   }
 }
-
-export default Car;
