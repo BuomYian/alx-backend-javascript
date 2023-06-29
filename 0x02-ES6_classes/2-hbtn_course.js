a2f0be545997d510d0150c2ddaea6e1e5519160c
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-06-29 20:35:38 +0300

    fixed

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,40 +1,52 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = typeof name === 'string' ? name : '';
-    this._length = typeof length === 'number' ? length : 0;
-    this._students = Array.isArray(students) ? students : [];
+    this._name = this.validateString(name, 'Name');
+    this._length = this.validateNumber(length, 'Length');
+    this._students = this.validateArray(students, 'Students');
   }
 
-  //   Getter and setter for name
+  validateString(value, attribute) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a string`);
+    }
+    return value;
+  }
+
+  validateNumber(value, attribute) {
+    if (typeof value !== 'number' || isNaN(value)) {
+      throw new TypeError(`${attribute} must be a number`);
+    }
+    return value;
+  }
+
+  validateArray(value, attribute) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${attribute} must be an array`);
+    }
+    return value;
+  }
+
   get name() {
     return this._name;
   }
 
   set name(newName) {
-    if (typeof newName === 'string') {
-      this._name = newName;
-    }
+    this._name = this.validateString(newName, 'Name');
   }
 
-  //   Getter and setter for length
   get length() {
     return this._length;
   }
 
   set length(newLength) {
-    if (typeof newLength === 'number') {
-      this._length = newLength;
-    }
+    this._length = this.validateNumber(newLength, 'Length');
   }
 
-  //   Getter and setter for students
   get students() {
     return this._students;
   }
 
   set students(newStudents) {
-    if (Array.isArray(newStudents)) {
-      this._students = newStudents;
-    }
+    this._students = this.validateArray(newStudents, 'Students');
   }
 }
