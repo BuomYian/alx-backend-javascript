3db668486b4f02562ed29a883921273be9ba7df3
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-06-30 09:48:55 +0300

    super

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  //   Setter and getter for each attribute
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    this._floors = newFloors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evaluate slowly the ${this._floors} floors`;
+  }
+}
