6953e7da56fd4b0b63f4704678ba130b3fab5f3a
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-01 13:20:10 +0300

    full income

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,16 @@
+import getBudgetObject from './7-getBudgetObject';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, income);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
