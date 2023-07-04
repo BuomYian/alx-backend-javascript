221aca420908ebab92d6f25071799a5ea4339346
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-07-04 15:21:59 +0300

    map

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+import getListStudents from './0-get_list_students';
+
+export default function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+
+  return arr.map((student) => student.id);
+}
