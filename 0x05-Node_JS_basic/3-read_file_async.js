fb32393b42d8092fb4082f772e65c26e57550cad
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-22 20:31:48 +0300

    more errors

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -8,47 +8,42 @@ function countStudents(path) {
         return;
       }
 
-      const trimmedData = data.trim();
-      const lines = trimmedData.split(/\r?\n/);
+      const lines = data.trim().split('\n');
+      const headers = lines[0].split(',');
 
-      if (lines.length === 0) {
-        reject(new Error('No valid student data found in the database'));
+      const students = {};
+      const fieldIndex = headers.findIndex(
+        (header) => header.trim() === 'field'
+      );
+      const nameIndex = headers.findIndex(
+        (header) => header.trim() === 'firstname'
+      );
+
+      if (fieldIndex === -1 || nameIndex === -1) {
+        reject(new Error('Invalid CSV format: missing required columns'));
         return;
       }
 
-      const headers = lines[0].split(',').map((header) => header.trim());
+      lines.slice(1).forEach((line) => {
+        const values = line.split(',');
+        const fieldName = values[fieldIndex].trim();
+        const name = values[nameIndex].trim();
 
-      const fieldIndexMap = {
-        firstname: headers.indexOf('firstname'),
-        field: headers.indexOf('field'),
-      };
+        if (!students[fieldName]) {
+          students[fieldName] = [];
+        }
+
+        students[fieldName].push(name);
+      });
 
       console.log(`Number of students: ${lines.length - 1}`);
-
-      const studentData = lines
-        .slice(1)
-        .map((line) => line.split(',').map((item) => item.trim()));
-
-      const studentsByField = {};
-
-      studentData.forEach((student) => {
-        const field = student[fieldIndexMap.field];
-        if (!studentsByField[field]) {
-          studentsByField[field] = [];
-        }
-        if (student[fieldIndexMap.firstname]) {
-          studentsByField[field].push(student[fieldIndexMap.firstname]);
-        }
-      });
-
-      Object.keys(studentsByField).forEach((field) => {
-        const studentList = studentsByField[field];
+      for (const field in students) {
         console.log(
           `Number of students in ${field}: ${
-            studentList.length
-          }. List: ${studentList.join(', ')}`
+            students[field].length
+          }. List: ${students[field].join(', ')}`
         );
-      });
+      }
 
       resolve();
     });
