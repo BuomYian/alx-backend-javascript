ecf7e40918dd5eec148a33cfb1b861ca4b52c22e
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-22 20:37:14 +0300

    express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!\n');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
+module.exports = app;
