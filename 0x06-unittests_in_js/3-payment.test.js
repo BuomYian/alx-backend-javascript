be84ecd91471171f55e94fe85461dacce44e606d
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-24 13:44:29 +0300

    stub

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,21 @@
 const sinon = require('sinon');
 const expect = require('chai').expect;
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', function () {
-  it('should call Utils.calculateNumber with the correct arguments and log the result', function () {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+  it('should stub Utils.calculateNumber and log the correct message', function () {
+    const calculateNumberStub = sinon
+      .stub(Utils, 'calculateNumber')
+      .returns(10);
     const consoleLogSpy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
-    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-    expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
 
-    calculateNumberSpy.restore();
+    calculateNumberStub.restore();
     consoleLogSpy.restore();
   });
 });
