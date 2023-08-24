7a0a3d9a296d7ad1f4ae901e43132ea6e0816937
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-24 20:56:29 +0300

    expect

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,5 +1,5 @@
 const sinon = require('sinon');
-const { expect } = require('chai').expect;
+const expect = require('chai').expect;
 const sendPaymentRequestToApi = require('./4-payment.js');
 const Utils = require('./utils.js');
 
