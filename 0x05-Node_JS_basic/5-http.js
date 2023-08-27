3146fc8742724d54b8177ce49e7b72d247c72e34
Buomkuoth Yian <buom.nyajal@gmail.com>
2023-08-27 14:16:07 +0300

    http

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,37 +1,112 @@
 const http = require('http');
-const url = require('url');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const PORT = 1245;
 const HOST = 'localhost';
 const app = http.createServer();
+const DB_FILE = process.argv.length > 2 ? process.argv[2] : '';
 
-app.on('request', (req, res) => {
-  if (req.method === 'GET') {
-    const { pathname, query } = url.parse(req.url, true);
+const countStudents = (path) =>
+  new Promise((resolve, reject) => {
+    if (!path) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (path) {
+      fs.readFile(path, (err, data) => {
+        if (err) {
+          reject(new Error('Cannot load the database'));
+        }
+        if (data) {
+          const reportParts = [];
+          const fileLines = data.toString('utf-8').trim().split('\n');
+          const studentGroups = {};
+          const dbFieldNames = fileLines[0].split(',');
+          const studentPropNames = dbFieldNames.slice(
+            0,
+            dbFieldNames.length - 1
+          );
 
-    if (pathname === '/') {
+          for (const line of fileLines.slice(1)) {
+            const studentRecord = line.split(',');
+            const studentPropValues = studentRecord.slice(
+              0,
+              studentRecord.length - 1
+            );
+            const field = studentRecord[studentRecord.length - 1];
+            if (!Object.keys(studentGroups).includes(field)) {
+              studentGroups[field] = [];
+            }
+            const studentEntries = studentPropNames.map((propName, idx) => [
+              propName,
+              studentPropValues[idx],
+            ]);
+            studentGroups[field].push(Object.fromEntries(studentEntries));
+          }
+
+          const totalStudents = Object.values(studentGroups).reduce(
+            (pre, cur) => (pre || []).length + cur.length
+          );
+          reportParts.push(`Number of students: ${totalStudents}`);
+          for (const [field, group] of Object.entries(studentGroups)) {
+            reportParts.push(
+              [
+                `Number of students in ${field}: ${group.length}.`,
+                'List:',
+                group.map((student) => student.firstname).join(', '),
+              ].join(' ')
+            );
+          }
+          resolve(reportParts.join('\n'));
+        }
+      });
+    }
+  });
+
+const SERVER_ROUTE_HANDLERS = [
+  {
+    route: '/',
+    handler(_, res) {
+      const responseText = 'Hello Holberton School!';
+
       res.setHeader('Content-Type', 'text/plain');
+      res.setHeader('Content-Length', responseText.length);
       res.statusCode = 200;
-      res.end('Hello Holberton School!\n');
-    } else if (pathname === '/students') {
-      const databaseFile = query.file || 'database.csv';
+      res.write(Buffer.from(responseText));
+    },
+  },
+  {
+    route: '/students',
+    handler(_, res) {
+      const responseParts = ['This is the list of our students'];
 
-      countStudents(databaseFile)
+      countStudents(DB_FILE)
         .then((report) => {
+          responseParts.push(report);
+          const responseText = responseParts.join('\n');
           res.setHeader('Content-Type', 'text/plain');
+          res.setHeader('Content-Length', responseText.length);
           res.statusCode = 200;
-          res.end(`This is the list of our students\n${report}`);
+          res.write(Buffer.from(responseText));
         })
         .catch((err) => {
+          responseParts.push(
+            err instanceof Error ? err.message : err.toString()
+          );
+          const responseText = responseParts.join('\n');
           res.setHeader('Content-Type', 'text/plain');
-          res.statusCode = 500;
-          res.end(`Error: ${err.message}\n`);
+          res.setHeader('Content-Length', responseText.length);
+          res.statusCode = 200;
+          res.write(Buffer.from(responseText));
         });
-    } else {
-      res.setHeader('Content-Type', 'text/plain');
-      res.statusCode = 404;
-      res.end('Not found\n');
+    },
+  },
+];
+
+app.on('request', (req, res) => {
+  for (const routeHandler of SERVER_ROUTE_HANDLERS) {
+    if (routeHandler.route === req.url) {
+      routeHandler.handler(req, res);
+      break;
     }
   }
 });
